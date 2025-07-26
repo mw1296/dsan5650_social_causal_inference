e8653967cdc818d2d3225eef86d090668b96c4ae
mw1296 <mw1296@georgetown.edu>
2025-07-26 17:14:03 -0400

    update index.qmd

--- final_project/backup/import pandas as pd.py
+++ final_project/backup/import pandas as pd.py
@@ -1,42 +1,42 @@
 import pandas as pd
 import os
 os.getcwd()
-df_dic = {}
-
-def create_df_race(folder_path, file_name, demographic_type, rows, id_vars, var_name, value_name):
-    file_path = os.path.join("final_project/data", folder_path, file_name + ".xlsx")    
-    df = pd.read_excel(file_path, skiprows=rows)
-    df_long = df.melt(id_vars=id_vars, var_name=var_name, value_name=value_name)
-    df_long[demographic_type] = file_name
-    df_dic[file_name] = df_long
-    return df_dic
 
 
-folder_path = ["unemployment_rate_by_race",
+def create_df_race(folder_path, demographic_type, id_vars, var_name, value_name):
+    merged_df = pd.DataFrame()
+    file_name = [f.removesuffix('.xlsx') for f in os.listdir(os.path.join("../data", folder_path)) if f.endswith('.xlsx')]
+    for f in file_name:
+        file_path = os.path.join("../data", folder_path, f + ".xlsx")    
+        df = pd.read_excel(file_path)
+        rows = df[df.iloc[:,0]=='Year'].index[0]+2-1
+        df = pd.read_excel(file_path, skiprows=rows)
+        df_long = df.melt(id_vars=id_vars, var_name=var_name, value_name=value_name)
+        df_long[demographic_type] = f
+        merged_df = pd.concat([merged_df, df_long], ignore_index=True)
+    return merged_df
+
+folder_path_list = ["unemployment_rate_by_race",
              "unemployment_rate_by_sex",
              "unemployment_rate_by_industry",
              "unemployment_rate_by_age",
-             "unemployment_rate_by_education_attainment"]
-demographic_type = ['Race', 'Sex', 'Industry', 'Age', 'Education_attainment']
+            "unemployment_rate_by_education_attainment"]
 
-for i in range(0, len(folder_path)):
-    print(folder_path[i])
-    file_name = [f.removesuffix('.xlsx') for f in os.listdir(os.path.join("final_project/data", folder_path[i])) if f.endswith('.xlsx')]
+demographic_type_list = ['Race',
+                    'Sex',
+                    'Industry',
+                    'Age',
+                    'Education_attainment']
 
-demographic_type = 'Race'
 d_vars="Year"
 var_name="Month"
 value_name="Unemployment_rate"
-rows = 12
-for f in file_name:
-    create_df_race(folder_path, f, demographic_type, rows, "Year", "Month", "Unemployment_rate")
 
+df_dic = {}
+for i in range(0, len(folder_path_list)):
+    folder_path = folder_path_list[i]
+    demographic_type=demographic_type_list[i]
+    merged_df = create_df_race(folder_path, demographic_type, "Year", "Month", "Unemployment_rate")
+    df_dic[demographic_type] = merged_df
 
-#file_path = os.path.join("final_project/data", folder_path, file_name + ".xlsx")
-# rows = 12
-# df = pd.read_excel(file_path, skiprows=rows)
-# df_long = df.melt(id_vars="Year", var_name="Month", value_name="Unemployment_rate")
-# df_long["Race"] = "white"
-# df_dic[file_name] = df_long
-
-
+list(df_dic.values())[4]
