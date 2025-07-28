38ffd6604ae87d737b79b4f12168d9ae08e2e5d7
mw1296 <mw1296@georgetown.edu>
2025-07-28 13:32:46 -0400

    complete weibull distribution analysis

--- final_project/backup/notebook_code_pk.py
+++ final_project/backup/notebook_code_pk.py
@@ -83,44 +83,19 @@ df_overall_long = df_overall.melt(id_vars="Year", var_name="Month", value_name="
 
 # Fit the log-norma distribution# Fit log-norm distribution using population unemployment rate data
 from scipy.stats import lognorm, kstest, skew
-
 ## Get the unemployment_rate data that is not NA, convert to array
-data_to_fit_lognorm = df_overall_long[~np.isnan(np.array(df_overall_long['Unemployment_rate']))]['Unemployment_rate']
-
+data_to_fit = df_overall_long[~np.isnan(np.array(df_overall_long['Unemployment_rate']))]['Unemployment_rate']
 ## Get the estimated parameters
-sigma_fit, loc_fit, scale_fit = lognorm.fit(data_to_fit_lognorm)
-
-## Get mu (mean of log-data) from scale
-mu_fit = np.log(scale_fit)
+sigma_fit, loc_fit, scale_fit = lognorm.fit(data_to_fit)
 print(f"\nFitted Log-Norm Distribution Parameters:")
 print(f"  Shape parameter 'alpha'): {sigma_fit:.2f}")
 print(f"  Location parameter (loc): {loc_fit:.2f}")
 print(f"  Scale parameter (scale): {scale_fit:.2f}")
-print(f"  mu (mean of log-data): {mu_fit:.2f}")
-
 # Visualize the fit
 import matplotlib.pyplot as plt
 fig, ax = plt.subplots(1, 1, figsize=(10, 9))
 ## Histogram of the unemployment data
-ax.hist(data_to_fit_lognorm, bins=30, density=True, alpha=0.6, color='skyblue', edgecolor='black', label='Data Histogram')
-## Fitted histogram
-xmin, xmax = ax.get_xlim()
-x = np.linspace(xmin, xmax, 100)
-p = lognorm.pdf(x, sigma_fit, loc=loc_fit, scale=scale_fit)
-ax.plot(x, p, 'darkred', lw=2, label='Fitted Log-Normal PDF')
-ax.set_xlabel('Unemployment Rate')
-ax.set_ylabel('Density')
-ax.set_title('Histogram with Fitted Log-Normal Distribution PDF')
-ax.legend()
-ax.grid(True, linestyle='--', alpha=0.7)
-plt.tight_layout()
-plt.show()
-
-
-import matplotlib.pyplot as plt
-fig, ax = plt.subplots(1, 1, figsize=(10, 9))
-## Histogram of the unemployment data
-ax.hist(data_to_fit_lognorm, bins=30, density=True, alpha=0.6, color='skyblue', edgecolor='black', label='Data Histogram')
+ax.hist(data_to_fit, bins=30, density=True, alpha=0.6, color='skyblue', edgecolor='black', label='Data Histogram')
 ## Fitted histogram
 xmin, xmax = ax.get_xlim()
 x = np.linspace(xmin, xmax, 100)
@@ -133,3 +108,94 @@ ax.grid(True, linestyle='--', alpha=0.7)
 plt.tight_layout()
 plt.suptitle("Data Histogram vs. Fitted Log-Normal Distribution PDF", y=1.02, fontsize=18)
 plt.show()
+
+
+# Fit gamma distribution using MLE
+from scipy.stats import gamma, skew, kstest
+alpha_fit, loc_fit, scale_fit = gamma.fit(data_to_fit) # fit gamma distribution
+# Print the estimated parameters
+print(f"\nFitted Gamma Distribution Parameters:")
+print(f"  Shape parameter 'alpha': {alpha_fit:.2f}")
+print(f"  Location parameter 'loc': {loc_fit:.2f}")
+print(f"  Scale parameter 'scale'): {scale_fit:.2f}")
+# Visualize the fit
+fig, ax = plt.subplots(1, 1, figsize = (10, 8))
+## Histogram of the unemployment data
+ax.hist(data_to_fit, bins = 20, density=True, color='skyblue', alpha= 0.7, edgecolor='black', label = 'Data Histogram')
+xmin, xmax = ax.get_xlim()
+x = np.linspace(xmin, xmax, 100)
+## Plot fitted gamma pdf
+p = gamma.pdf(x, alpha_fit, loc=loc_fit, scale=scale_fit)
+ax.plot(x, p, 'darkred', lw=2, label='Fitted Gamma PDF')
+ax.set_xlabel('Unemployment Rate', fontsize=10)
+ax.set_ylabel('Density', fontsize=10)
+ax.legend()
+ax.grid(True, linestyle='--', alpha=0.7)
+plt.tight_layout()
+plt.suptitle("Data Histogram vs. Fitted Gamma Distribution PDF", y=1.02, fontsize=18)
+plt.show()
+# Check the p value of fitted gamma distribution
+d_statistic, p_value = kstest(data_to_fit, lambda x: gamma.cdf(x, alpha_fit, loc=loc_fit, scale=scale_fit))
+print(f"\nKolmogorov-Smirnov Test Results:")
+print(f"  D-statistic: {d_statistic:.4f}")
+print(f"  P-value: {p_value:.4f}")
+
+
+# Fit Weibull distribution using MLE
+from scipy.stats import weibull_min, skew, kstest
+from scipy.special import gamma as gamma_func # For Weibull mean calculation
+c_fit, loc_fit, scale_fit = weibull_min.fit(data_to_fit) # fit weibull distribution
+# Print the estimated parameters
+print(f"\nFitted Weibull Distribution Parameters:")
+print(f"  Shape parameter 'c' (k): {c_fit:.4f}")
+print(f"  Location parameter (loc): {loc_fit:.4f}")
+print(f"  Scale parameter (scale, lambda): {scale_fit:.4f}")
+# Visualize the fit
+fig, ax = plt.subplots(1, 1, figsize = (10, 8))
+## Histogram of the unemployment data
+ax.hist(data_to_fit, bins = 20, density=True, color='skyblue', alpha= 0.7, edgecolor='black', label = 'Data Histogram')
+xmin, xmax = ax.get_xlim()
+x = np.linspace(xmin, xmax, 100)
+## Plot fitted gamma pdf
+p = weibull_min.pdf(x, c_fit, loc=loc_fit, scale=scale_fit)
+ax.plot(x, p, 'darkred', lw=2, label='Fitted Weibull PDF')
+ax.set_xlabel('Unemployment Rate', fontsize=10)
+ax.set_ylabel('Density', fontsize=10)
+ax.legend()
+ax.grid(True, linestyle='--', alpha=0.7)
+plt.tight_layout()
+plt.suptitle("Data Histogram vs. Fitted Weibull Distribution PDF", y=1.02, fontsize=18)
+plt.show()
+# Fit the Weibull
+d_statistic, p_value = kstest(data_to_fit, lambda x: weibull_min.cdf(x, c_fit, loc=loc_fit, scale=scale_fit))
+print(f"\nKolmogorov-Smirnov Test Results:")
+print(f"  D-statistic: {d_statistic:.4f}")
+print(f"  P-value: {p_value:.4f}")
+# Calculate inferred mean
+fitted_mean = loc_fit + scale_fit * gamma_func(1 + 1/c_fit)
+
+#| tbl-cap: "Weibull Distribution and Fitness Statistics"
+#| tbl-col: true
+#| tbl-center: true
+# Print out the Weibull stats
+# Print the weibull stats
+weibull_stats = {
+  "parameters": [
+    "Shape (c)",
+    "Location (loc)" ,
+    "Scale",
+    "D_statistics",
+    "P-value",
+    "Fitted Mean"
+  ],
+  "values": [
+     round(c_fit, 4),
+     round(loc_fit, 4),
+     round(scale_fit, 4),
+     round(d_statistic, 4),
+     round(p_value, 4),
+     round(fitted_mean, 2)
+  ]
+}
+weibull_stats_table = pd.DataFrame(weibull_stats, index=False)
+display(weibull_stats_table.style.hide(axis='index'))
